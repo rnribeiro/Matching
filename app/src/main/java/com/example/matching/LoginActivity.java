f59204d5a0b2059cde16202a53bec6c13ade3aa5
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2022-05-06 09:51:16 +0100

    Revert "Rent activity"
    This reverts commit 25d987fe075e67517bc0d22c556e6410c243d953.

--- app/src/main/java/com/example/matching/LoginActivity.java
+++ app/src/main/java/com/example/matching/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.example.matching;
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,16 +9,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Button do_login_btn = findViewById(R.id.do_login_btn);
-        do_login_btn.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent do_login = new Intent(getApplicationContext(), RentActivity.class);
-                do_login.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(do_login);
-                overridePendingTransition(0,0);
-            }
-        });
     }
 }
