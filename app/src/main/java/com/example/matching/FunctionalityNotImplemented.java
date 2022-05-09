56fc844210ec2bf1cb3f04bba9bcc1d519f4576b
beatrizfonseca251 <r20201599@novaims.unl.pt>
2022-05-09 17:22:45 +0100

    Added following activities: - Functionality not implemented - Successful - Pay using

---
+++ app/src/main/java/com/example/matching/FunctionalityNotImplemented.java
@@ -0,0 +1,13 @@
+package com.example.matching;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FunctionalityNotImplemented extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_functionality_not_implemented);
+    }
+}
