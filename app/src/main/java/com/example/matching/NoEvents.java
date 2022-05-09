d888abd41299b41279c3426288a794aa3f476784
beatrizfonseca251 <r20201599@novaims.unl.pt>
2022-05-09 18:25:49 +0100

    Added following activities: - Friend Events - No events

---
+++ app/src/main/java/com/example/matching/NoEvents.java
@@ -0,0 +1,13 @@
+package com.example.matching;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_events);
+    }
+}
